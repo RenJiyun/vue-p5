de73aa1db31a2cb788100bd4f728f7616cc67919
renjiyun <wozaijixu@msn.cn>
2021-03-28 17:34:30 +0800

    初始化

---
+++ src/components/p5/P5Test.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="canvas"></div>
+</template>
+
+<script>
+import P5 from "p5";
+export default {
+  name: "P5Test",
+  mounted() {
+    const script = (p5) => {
+      (p5.setup = () => {
+        this.setup(p5);
+      }),
+        (p5.draw = () => {
+          this.draw(p5);
+        });
+    };
+    new P5(script, "canvas");
+  },
+  methods: {
+    setup(p5) {
+      p5.createCanvas(p5.windowWidth, p5.windowHeight);
+    },
+
+    draw(p5) {
+      p5.ellipse(p5.mouseX, p5.mouseY, 50, 50);
+    },
+  },
+};
+</script>
