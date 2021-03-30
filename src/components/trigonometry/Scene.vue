48a09237d57b9a1a73d0e85134506434c89b4c48
renjiyun <wozaijixu@msn.cn>
2021-03-30 23:31:03 +0800

    坐标系加入一些控制

--- src/components/trigonometry/Scene.vue
+++ src/components/trigonometry/Scene.vue
@@ -20,6 +20,12 @@ export default {
       }),
         (p5.draw = () => {
           this.draw(p5);
+        }),
+        (p5.keyPressed = () => {
+          this.keyPressed(p5);
+        }),
+        (p5.mouseWheel = (e) => {
+          this.mouseWheel(p5, e);
         });
     };
     new P5(script, "canvas");
@@ -28,6 +34,7 @@ export default {
   data() {
     return {
       coord: null,
+      scale: 5000,
     };
   },
 
@@ -35,6 +42,7 @@ export default {
     setup(p5) {
       p5.createCanvas(1280, 720);
       let coordConfig = {
+        defaultCanvas: p5,
         ox: 0,
         oy: 0,
         width: p5.width,
@@ -48,38 +56,64 @@ export default {
       this.f = new Function((x) => x * x);
       this.triangle = new Triangle(-6, 0, 0, 6, 4, 0);
 
-
       this.rollingCircle1 = new RollingCircle(1, 2, (l) => {
         return {
           c: $math.complex(l, 0),
-          nc: $math.complex(0, 1)
-        }
-      })
+          nc: $math.complex(0, 1),
+        };
+      });
 
       this.rollingCircle2 = new RollingCircle(2, 2, (l) => {
         return {
-          c: $math.complex({r: l, phi: $math.pi / 4}),
-          nc: $math.complex({r: 1, phi: $math.pi * 3 / 4})
-        }
-      })
+          c: $math.complex({ r: l, phi: $math.pi / 4 }),
+          nc: $math.complex({ r: 1, phi: ($math.pi * 3) / 4 }),
+        };
+      });
+
+      this.rollingCircle3 = new RollingCircle(10, 6, () => {
+        return {
+          c: $math.complex(-5, 0),
+          nc: $math.complex(0, 0),
+        };
+      });
 
       this.animationChain = new Chain([
-        new Create(this.coord, 500),
-        new Create(this.circle, 500),
-        new Create(this.f, 500),
-        new Create(this.triangle, 500),
-        new Create(this.rollingCircle1, 100),
-        new Create(this.rollingCircle2, 100)
+        // new Create(this.coord, 500),
+        // new Create(this.circle, 500),
+        // new Create(this.f, 500),
+        // new Create(this.triangle, 500),
+        // new Create(this.rollingCircle1, 100),
+        new Create(this.rollingCircle2, 5000),
       ]);
     },
 
     draw(p5) {
       p5.background(0);
       p5.translate(p5.width / 2, p5.height / 2);
-      // this.coord.show(p5);
+      this.coord.show();
       // this.animationChain.display(p5, this.coord)
-      // this.coord.showMobj(p5, this.rollingCircle);
+      this.coord.showMobj(p5, this.rollingCircle3);
     },
+
+    keyPressed(p5) {
+      if (p5.keyCode == 71) {
+        this.coord.grid = !this.coord.grid;
+      }
+    },
+
+    mouseWheel(p5, e) {
+      let delta = e.delta;
+      this.scale = this.scale - delta
+      if (this.scale >= 5000) {
+        this.scale = 5000;
+      }
+
+      if (this.scale <= 2500) {
+        this.scale = 2500
+      }
+      this.coord.scale = this.scale / 5000;
+      console.log(this.coord.scale)
+    }
   },
 };
 </script>
--- src/lib/CoordinateSystem.js
+++ src/lib/CoordinateSystem.js
@@ -1,23 +1,93 @@
-
 const $math = require("mathjs");
 
-function CoordinateSystem({ ox, oy, width, height, xInterval, yInterval, grid, labelInterval }) {
-    this.ox = ox;
-    this.oy = oy;
-    this.width = width;
-    this.height = height;
-    this.xInterval = xInterval;
-    this.yInterval = yInterval ? yInterval : xInterval;
-    this.grid = grid == undefined ? true : grid;
-    this.xMax = this.width / this.xInterval / 2;
-    this.xMin = - this.xMax;
-    this.yMax = this.height / this.yInterval / 2;
-    this.yMin = -this.yMax
-    this.labelInterval = labelInterval == undefined ? 1 : labelInterval;
+class CoordinateSystem {
+    constructor(config) {
+        let { defaultCanvas, ox, oy, width, height, xInterval, yInterval, grid, labelInterval } = config;
+        this.defaultCanvas = defaultCanvas;
+        this.ox = ox;
+        this.oy = oy;
+        this._width = width;
+        this._height = height;
+        this._xInterval = xInterval;
+        this._yInterval = yInterval ? yInterval : xInterval;
+        this._grid = grid == undefined ? true : grid;
+        this.xMax = this.width / this.xInterval / 2;
+        this.xMin = - this.xMax;
+        this.yMax = this.height / this.yInterval / 2;
+        this.yMin = -this.yMax
+        this._labelInterval = labelInterval == undefined ? 1 : labelInterval;
+        this._scale = 1;
+    }
 
 
+    updateConfig() {
+        this.xMax = this.width / this.xInterval / 2;
+        this.xMin = - this.xMax;
+        this.yMax = this.height / this.yInterval / 2;
+        this.yMin = -this.yMax
+    }
 
-    this.showMobj = (canvas, mobj) => {
+
+    get labelInterval() {
+        return Math.ceil(this._labelInterval / this._scale);
+    }
+
+    get width() {
+        return this._width * this.scale;
+    }
+
+    set width(w) {
+        this._width = w;
+        this.updateConfig();
+    }
+
+    get height() {
+        return this._height * this.scale;
+    }
+
+    set height(h) {
+        this._height = h;
+        this.updateConfig();
+    }
+
+    get xInterval() {
+        return this._xInterval * this.scale;
+
+    }
+
+    set xInterval(xi) {
+        this._xInterval = xi;
+        this.updateConfig();
+    }
+
+    get yInterval() {
+        return this._yInterval * this.scale;
+    }
+
+    set yInterval(yi) {
+        this._yInterval = yi;
+        this.updateConfig();
+    }
+
+
+    get grid() {
+        return this._grid;
+    }
+
+    set grid(flag) {
+        this._grid = flag;
+    }
+
+    get scale() {
+        return this._scale;
+    }
+
+    set scale(s) {
+        this._scale = s;
+    }
+
+
+    showMobj(canvas = this.defaultCanvas, mobj) {
         canvas.noFill();
         canvas.stroke(255);
         let env = {
@@ -48,12 +118,12 @@ function CoordinateSystem({ ox, oy, width, height, xInterval, yInterval, grid, l
     }
 
 
-    this.outOfRange = (p) => {
+    outOfRange(p) {
         return p.x > this.xMax || p.x < this.xMin || p.y > this.yMax || p.y < this.yMin;
     }
 
 
-    this.toOuterCoord = (p) => {
+    toOuterCoord(p) {
         return {
             x: this.ox + p.x * this.xInterval,
             y: this.oy - p.y * this.yInterval
@@ -61,85 +131,31 @@ function CoordinateSystem({ ox, oy, width, height, xInterval, yInterval, grid, l
     }
 
 
-    // 圆
-    this.circle = (p5, x0, y0, r) => {
-        let x = (this.ox + x0) * this.xInterval;
-        let y = (this.oy - y0) * this.yInterval;
-        p5.circle(x, y, 2 * r * xInterval);
-    }
-
-    // 圆周运动
-    this.circularMotion = (p5, x0, y0, r) => {
-        let ret = [];
-        p5.noFill();
-        p5.stroke(255);
-        p5.beginShape();
-        let delta = 0.1;
-        let max = Math.ceil($math.pi * 2 / delta) + 5;
-        let iterNum = p5.frameCount % max;
-        for (let i = 0; i < iterNum; i++) {
-            let x = x0 + $math.cos(i * delta) * r;
-            let y = y0 + $math.sin(i * delta) * r;
-            let p = this.toOuterCoord({ x: x, y: y });
-            ret.push({
-                x: x, y: y
-            })
-            p5.vertex(p.x, p.y);
-        }
-        p5.endShape();
-        return ret;
-    }
-
-    // 函数图像
-    this.showFunctions = (p5, fs) => {
-        let interval = 0.1;
-        p5.noFill();
-        p5.stroke(255);
-        for (let f of fs) {
-            p5.beginShape();
-            for (let x = this.xMin; x <= this.xMax; x += interval) {
-                let y = f(x);
-                if (this.outOfRange({ x: x, y: y })) continue;
-                p5.vertex(this.ox + x * this.xInterval, this.oy - y * this.yInterval);
-            }
-            p5.endShape();
-        }
-    }
-
-
-    // 点
-    this.showPoints = (p5, points) => {
-        for (let p of points) {
-            p5.circle(this.ox + p.x * this.xInterval, this.oy - p.y * this.yInterval, 6);
-        }
-    }
-
-
     // 显示复数
-    this.showComplexes = (p5, complexes, arrow = true, label = false) => {
+    showComplexes(canvas = this.defaultCanvas, complexes, arrow = true, label = false) {
         let arrowLen = Math.min(this.width, this.height) / 80;
 
         for (let c of complexes) {
-            p5.stroke(255);
-            p5.fill(255);
+            canvas.stroke(255);
+            canvas.fill(255);
             if (arrow) {
-                p5.line(this.ox, this.oy, this.ox + c.re * this.xInterval, this.oy - c.im * this.yInterval);
+                canvas.line(this.ox, this.oy, this.ox + c.re * this.xInterval, this.oy - c.im * this.yInterval);
 
                 let rc1 = $math.complex({ r: 1, phi: $math.pi * 4 / 5 }).mul($math.complex({ r: arrowLen, phi: c.arg() })).add(c);
                 let rc2 = $math.complex({ r: 1, phi: -$math.pi * 4 / 5 }).mul($math.complex({ r: arrowLen, phi: c.arg() })).add(c);
-                p5.beginShape();
-                p5.vertex(this.ox + rc1.re * this.xInterval, this.oy - rc1.im * this.yInterval);
-                p5.vertex(this.ox + c.re * this.xInterval, this.oy - c.im * this.yInterval);
-                p5.vertex(this.ox + rc2.re * this.xInterval, this.oy - rc2.im * this.yInterval);
-                p5.endShape(p5.CLOSE);
+                canvas.beginShape();
+                canvas.vertex(this.ox + rc1.re * this.xInterval, this.oy - rc1.im * this.yInterval);
+                canvas.vertex(this.ox + c.re * this.xInterval, this.oy - c.im * this.yInterval);
+                canvas.vertex(this.ox + rc2.re * this.xInterval, this.oy - rc2.im * this.yInterval);
+                canvas.endShape(canvas.CLOSE);
             } else {
-                p5.circle(this.ox + c.re * this.xInterval, this.oy - c.im * this.yInterval, 6);
+                canvas.circle(this.ox + c.re * this.xInterval, this.oy - c.im * this.yInterval, 6);
             }
 
             // 显示标签
             if (label) {
-                p5.noStroke();
-                p5.text(c.format(2),
+                canvas.noStroke();
+                canvas.text(c.format(2),
                     this.ox + c.re * this.xInterval,
                     this.oy - c.im * this.yInterval);
             }
@@ -147,42 +163,28 @@ function CoordinateSystem({ ox, oy, width, height, xInterval, yInterval, grid, l
         }
     }
 
-
-    this.shape = (p5, vertexes) => {
-        p5.noFill();
-        p5.stroke(255);
-        p5.beginShape()
-        for (let v of vertexes) {
-            let p = this.toOuterCoord({ x: v.re, y: v.im })
-            p5.vertex(p.x, p.y);
-        }
-        p5.endShape();
-    }
-
-
-    // 坐标系
-    this.show = (p5) => {
-        p5.stroke(255);
-        p5.fill(255);
+    show(canvas = this.defaultCanvas) {
+        canvas.stroke(255);
+        canvas.fill(255);
 
         // x-axis 
-        p5.line(this.ox - this.width / 2, this.oy, this.ox + this.width / 2, this.oy);
+        canvas.line(this.ox - this.width / 2, this.oy, this.ox + this.width / 2, this.oy);
         // y-axis
-        p5.line(this.ox, this.oy - this.height / 2, this.ox, this.oy + this.height / 2);
+        canvas.line(this.ox, this.oy - this.height / 2, this.ox, this.oy + this.height / 2);
 
 
-        p5.stroke(255, 100);
+        canvas.stroke(255, 100);
         for (let x = 0, i = 0; x <= this.width / 2; x += this.xInterval, i++) {
             if (this.grid) {
-                p5.line(this.ox + x, this.oy - this.height / 2, this.ox + x, this.oy + this.height / 2);
-                p5.line(this.ox - x, this.oy - height / 2, this.ox - x, this.oy + this.height / 2);
+                canvas.line(this.ox + x, this.oy - this.height / 2, this.ox + x, this.oy + this.height / 2);
+                canvas.line(this.ox - x, this.oy - this.height / 2, this.ox - x, this.oy + this.height / 2);
             }
 
             if (i % this.labelInterval == 0) {
-                p5.text(i, this.ox + x + 5, this.oy + 15);
-                p5.text(-i, this.ox - x + 5, this.oy + 15);
-                p5.circle(this.ox + x, this.oy, 5);
-                p5.circle(this.ox - x, this.oy, 5);
+                canvas.text(i, this.ox + x + 5, this.oy + 15);
+                canvas.text(-i, this.ox - x + 5, this.oy + 15);
+                canvas.circle(this.ox + x, this.oy, 5);
+                canvas.circle(this.ox - x, this.oy, 5);
             }
 
 
@@ -190,20 +192,20 @@ function CoordinateSystem({ ox, oy, width, height, xInterval, yInterval, grid, l
 
         for (let y = 0, i = 0; y <= this.height / 2; y += this.yInterval, i++) {
             if (this.grid) {
-                p5.line(this.ox - this.width / 2, this.oy + y, this.ox + this.width / 2, this.oy + y);
-                p5.line(this.ox - this.width / 2, this.oy - y, this.ox + this.width / 2, this.oy - y);
+                canvas.line(this.ox - this.width / 2, this.oy + y, this.ox + this.width / 2, this.oy + y);
+                canvas.line(this.ox - this.width / 2, this.oy - y, this.ox + this.width / 2, this.oy - y);
             }
 
             if (i != 0 && i % this.labelInterval == 0) {
-                p5.text(i, this.ox - 15, this.oy - y - 5);
-                p5.text(-i, this.ox - 20, this.oy + y - 5);
-                p5.circle(this.ox, this.oy - y, 5);
-                p5.circle(this.ox, this.oy + y, 5);
+                canvas.text(i, this.ox - 15, this.oy - y - 5);
+                canvas.text(-i, this.ox - 20, this.oy + y - 5);
+                canvas.circle(this.ox, this.oy - y, 5);
+                canvas.circle(this.ox, this.oy + y, 5);
             }
 
         }
 
-    };
+    }
 }
 
 export {
